6b84c0863b1ec763b2702fba9a30310ed13c3ab2
Ítalo Penha <italopenha77@outlook.com>
2023-10-27 18:14:56 -0300

    Adicionando método construtor na classe Produto em Exe03

--- Exe03/Produto.cs
+++ Exe03/Produto.cs
@@ -12,6 +12,13 @@ namespace Exe03
         public double Preco;
         public int Quantidade;
 
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
