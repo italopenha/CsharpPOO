243283fa8580e1c6c58f7424aa8a8042cfdcdcb7
Ítalo Penha <italopenha77@outlook.com>
2023-10-27 18:13:39 -0300

    Adicionando exercícios

---
+++ Exe07/ConversorDeMoedas.cs
@@ -0,0 +1,14 @@
+﻿namespace Exe07
+{
+    internal class ConversorDeMoedas
+    {
+        public static double Iof = 0.06;
+
+        public static double Converter(double cotacao, double dolares)
+        {
+            double conversao = cotacao * dolares;
+            double iof = conversao * Iof;
+            return conversao + iof;
+        }
+    }
+}
---
+++ Exe07/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exe07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolaresComprados = double.Parse(Console.ReadLine());
+
+            double conversao = ConversorDeMoedas.Converter(cotacao, dolaresComprados);
+
+            Console.WriteLine($"Valor a ser pago em reais = {conversao:F2}");
+        }
+    }
+}
