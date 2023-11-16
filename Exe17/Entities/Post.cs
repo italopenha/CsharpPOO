b93933585fe98c067332624c9c07fe52a49c3fbb
Ítalo Penha <italopenha77@outlook.com>
2023-11-16 14:36:34 -0300

    Configurado método ToString na classe Post

--- Exe17/Entities/Post.cs
+++ Exe17/Entities/Post.cs
@@ -1,4 +1,6 @@
-﻿namespace Exe17.Entities
+﻿using System.Text;
+
+namespace Exe17.Entities
 {
     internal class Post
     {
@@ -26,6 +28,23 @@
         public void RemoveComment(Comment comment)
         {
             Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
         }
     }
 }
