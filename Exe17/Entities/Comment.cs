dde091e5b1d7d1709f76e2f51628a8b5d0fdd834
Ítalo Penha <italopenha77@outlook.com>
2023-11-16 14:08:59 -0300

    Exe17: Criado a classe Comment

---
+++ Exe17/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Exe17.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
