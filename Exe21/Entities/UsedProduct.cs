0b916acb33a719173767c553cf9f3b2e9634631f
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 20:16:01 -0300

    Exe21: Criada a classe UsedProduct

---
+++ Exe21/Entities/UsedProduct.cs
@@ -0,0 +1,19 @@
+﻿namespace Exe21.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime Date { get; set; }
+
+        public UsedProduct() { }
+
+        public UsedProduct(string name, double price, DateTime date) : base(name, price)
+        {
+            Date = date;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} (used) $ {Price:F2} (Manufacture date: {Date:d})";
+        }
+    }
+}
