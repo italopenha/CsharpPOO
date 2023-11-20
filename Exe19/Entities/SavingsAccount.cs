f0523ab911247b89e8b43841996a923ac959718f
Ítalo Penha <italopenha77@outlook.com>
2023-11-19 21:48:15 -0300

    Adicionado exercício sobre herança

---
+++ Exe19/Entities/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace Exe19.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRating { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRating) : base(number, holder, balance)
+        {
+            InterestRating = interestRating;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRating;
+        }
+    }
+}
