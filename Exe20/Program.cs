b8f418cd38d78a42f812b35f61f8552f9b68c2d1
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 16:40:06 -0300

    Exe20: Criada a classe Program

--- Exe20/Program.cs
+++ Exe20/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Exe20
+﻿using Exe20.Entities;
+
+namespace Exe20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Digite o número de empregados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Dados do funcionário #{i}:");
+                Console.Write("Terceirizado (s/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Horas: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                if (ch == 's' || ch == 'S')
+                {
+                    Console.Write("Despesa adicional: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    list.Add(new OutsorcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }   
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("PAGAMENTOS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2")}");
+            }
         }
     }
 }
