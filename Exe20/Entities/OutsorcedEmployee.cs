4b7aac9c7d6d09a0c49d4e87ea7fef4ca70e426a
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 16:23:37 -0300

    Exe20: Criada a classe OutsorcedEmployee

---
+++ Exe20/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,19 @@
+﻿namespace Exe20.Entities
+{
+    internal class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee() { }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
