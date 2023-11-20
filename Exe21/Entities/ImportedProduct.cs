f28619c64ebd25ed71a0425a8cf242d64ca22148
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 20:10:06 -0300

    Exe21: Criada a classe ImportedProduct

---
+++ Exe21/Entities/ImportedProduct.cs
@@ -0,0 +1,24 @@
+﻿namespace Exe21.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} $ {Price:F2} (Customs fee: $ {CustomsFee:F2})";
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+    }
+}
