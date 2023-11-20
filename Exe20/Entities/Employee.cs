fca8bb952f4e031cad1e7e6b396d67332a42d48e
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 16:16:26 -0300

    Exe20: Criada a classe Employee

---
+++ Exe20/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Exe20.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
