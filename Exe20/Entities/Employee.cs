b933451f46a124186966aaa9064c5b482e9efb4d
Ítalo Penha <italopenha77@outlook.com>
2023-11-20 16:22:42 -0300

    Adicionada a palavra virtual ao método Payment na classe Employee

--- Exe20/Entities/Employee.cs
+++ Exe20/Entities/Employee.cs
@@ -15,7 +15,7 @@
             ValuePerHour = valuePerHour;
         }
 
-        public double Payment()
+        public virtual double Payment()
         {
             return Hours * ValuePerHour;
         }
