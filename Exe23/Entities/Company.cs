a5f38163e6aa2078cf54450c5f1ff9aa78aee9d6
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 19:07:17 -0300

    Exe23: Adicionado métodos construtores nas classes Company e Individual

--- Exe23/Entities/Company.cs
+++ Exe23/Entities/Company.cs
@@ -3,6 +3,14 @@
     internal class Company : TaxPayer
     {
         public int NumberOfEmployees { get; private set; }
+
+        public Company() { }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
         public override double Tax()
         {
             throw new NotImplementedException();
--- Exe23/Entities/Individual.cs
+++ Exe23/Entities/Individual.cs
@@ -3,6 +3,14 @@
     internal class Individual : TaxPayer
     {
         public double HealthExpenditures { get; private set; }
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
         public override double Tax()
         {
             throw new NotImplementedException();
