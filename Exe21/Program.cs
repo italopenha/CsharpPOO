c7374ec6d4a48d89ffa244a056f01899331b39f0
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 16:24:58 -0300

    Exe21: Criada a classe Program

--- Exe21/Program.cs
+++ Exe21/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Exe21
+﻿using Exe21.Entities;
+
+namespace Exe21
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<Product> products = new List<Product>();
+
+            Console.Write("Digite o número de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Dados do produto #{i}:");
+                Console.Write("Comum, usado ou importado (c/u/i)? ");
+                char productType = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Preço: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (productType == 'u' || productType == 'U')
+                {
+                    Console.Write("Data de fabricação (DD/MM/AAAA): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else if (productType == 'i' || productType == 'I')
+                {
+                    Console.Write("Taxa de alfândega: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Etiquetas de preço:");
+            foreach(Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
         }
     }
 }
