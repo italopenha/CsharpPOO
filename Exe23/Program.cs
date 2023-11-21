b1d1d82afd42a91eef823dff7bd6281fc893559c
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 19:43:09 -0300

    Exe23: Implementada a lógica do método Main na classe Program

--- Exe23/Program.cs
+++ Exe23/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Exe23
+﻿using Exe23.Entities;
+
+namespace Exe23
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<TaxPayer> taxPayers = new List<TaxPayer>();
+
+            Console.Write("Digite o número de contribuintes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Digite os dados do contribuinte #{i}:");
+                Console.Write("Pessoa física ou jurídica (f/j)? ");
+                char taxPayerType = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Rendimento anual: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+
+                if (taxPayerType == 'f' || taxPayerType == 'F')
+                {
+                    Console.Write("Despesas com saúde: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+                    taxPayers.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else if (taxPayerType == 'j' || taxPayerType == 'j')
+                {
+                    Console.Write("Número de funcionários: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    taxPayers.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            double sum = 0.0;
+
+            Console.WriteLine();
+            Console.WriteLine("Taxas pagas:");
+            foreach (TaxPayer taxPayer in taxPayers)
+            {
+                Console.WriteLine(taxPayer);
+                sum += taxPayer.Tax();
+            }
+
+            Console.WriteLine();
+            Console.Write("Total de imposto pago: ");
+            Console.WriteLine($"$ {sum:F2}");
         }
     }
 }
