ee4881769de6b648cee4d1f431b2d84989adec5e
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 18:48:52 -0300

    Exe23: Criada a classe Individual

---
+++ Exe23/Entities/Individual.cs
@@ -0,0 +1,11 @@
+﻿namespace Exe23.Entities
+{
+    internal class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+        public override double Tax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
