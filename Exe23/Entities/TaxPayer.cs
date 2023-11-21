22eca7801445e6eecdec93d3cb822c013a18cf65
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 18:42:12 -0300

    Exe23: Criada a classe TaxPayer

---
+++ Exe23/Entities/TaxPayer.cs
@@ -0,0 +1,18 @@
+﻿namespace Exe23.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; private set; }
+        public double AnualIncome { get; private set; }
+
+        public TaxPayer() { }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}
