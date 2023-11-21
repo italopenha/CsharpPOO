d8105afd325ac11359a2bca88efc7d17e9d9d7cf
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 18:53:27 -0300

    Exe23: Criada a classe Company

---
+++ Exe23/Entities/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace Exe23.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; private set; }
+        public override double Tax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
