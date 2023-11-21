90ffd457472ca0c9b3240ef3c145a89dea72e826
Ítalo Penha <italopenha77@outlook.com>
2023-11-21 19:31:51 -0300

    Exe23: Implementado método Tax na classe Company

--- Exe23/Entities/Company.cs
+++ Exe23/Entities/Company.cs
@@ -13,7 +13,21 @@
 
         public override double Tax()
         {
-            throw new NotImplementedException();
+            double rate = 0.0;
+            double tax = 0.0;
+
+            switch (NumberOfEmployees)
+            {
+                case <= 10:
+                    rate = 0.16;
+                    tax = AnualIncome * rate;
+                    break;
+                case > 10:
+                    rate = 0.14;
+                    tax = AnualIncome * rate;
+                    break;
+            }
+            return tax;
         }
     }
 }
