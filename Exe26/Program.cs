d667af900d271e1b2f530fabed27a9e9f3b931cf
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 17:32:58 -0300

    Exe26: Criada a lógica do método Main na classe Program

--- Exe26/Program.cs
+++ Exe26/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Exe26
+﻿using Exe26.Entities;
+using Exe26.Entities.Exceptions;
+
+namespace Exe26
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Digite os dados da conta:");
+                Console.Write("Número: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string holder = Console.ReadLine();
+                Console.Write("Saldo inicial: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Limite de saque: ");
+                double withDrawLimit = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Account acc = new Account(number, holder, balance, withDrawLimit);
+
+                Console.Write("Digite uma quantia para sacar: ");
+                double amount = double.Parse(Console.ReadLine());
+
+                acc.WithDraw(amount);
+
+                Console.Write($"Novo saldo: {acc.Balance:F2}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
         }
     }
 }
