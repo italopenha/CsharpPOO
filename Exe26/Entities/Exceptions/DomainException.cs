7fe99a6fe2cb6dd0ab23c466642d7f6a38306205
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 17:32:15 -0300

    Exe26: Criada a classe DomainException

---
+++ Exe26/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Exe26.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
