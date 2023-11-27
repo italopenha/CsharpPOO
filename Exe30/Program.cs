f991dd48d271ed03656e46527f75bcc4362bd7ed
Ítalo Penha <italopenha77@outlook.com>
2023-11-27 17:36:18 -0300

    Adicionado exemplo de uso do StreamWriter

---
+++ Exe30/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exe30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"c:\temp\file1.txt";
+            string targetpath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using(StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
