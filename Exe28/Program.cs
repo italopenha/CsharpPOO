ed501cefbb51e575fda6eabd1dc6df3fe3043a40
Ítalo Penha <italopenha77@outlook.com>
2023-11-27 17:34:38 -0300

    Adicionado exemplo de uso do FileStream e StreamReader

---
+++ Exe28/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exe28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                //sr = File.OpenText(path); alternativa ao filestream
+                while(!sr.EndOfStream) // para ler o arquivo inteiro
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
