ad8e1b7a4a4a78d2de28a1b8c3acd63fe5570750
Ítalo Penha <italopenha77@outlook.com>
2023-11-27 19:34:34 -0300

    Adicionado exercício de fixação da seção 13 - Trabalhando com arquivos

---
+++ Exe33/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Exe33
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{TotalPrice():F2}";
+        }
+    }
+}
---
+++ Exe33/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exe33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sourcePath = @"c:\dev\temp\file1.csv";
+                Directory.CreateDirectory(@"c:\dev\temp\out");
+                string targetPath = @"c:\dev\temp\out\summary.csv";
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(';');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
