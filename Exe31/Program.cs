781b0c2a9b8b87a9b26b520374987804e6f70566
Ítalo Penha <italopenha77@outlook.com>
2023-11-27 17:37:19 -0300

    Adicionado exemplo de uso do Directory

---
+++ Exe31/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exe31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder2");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
