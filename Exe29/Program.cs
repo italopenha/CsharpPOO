8301dde5f237c93a2ad7a0694e3ecdac5c93614f
Ítalo Penha <italopenha77@outlook.com>
2023-11-27 17:35:30 -0300

    Adicionado exemplo de uso do StreamReader

---
+++ Exe29/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exe29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
