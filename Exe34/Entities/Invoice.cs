bc8fff2bca29e399482ddac9a34a921c325b9416
Italo <italopenha77@outlook.com>
2023-12-14 20:48:59 -0300

    Adicionado exercício de exemplo de solução sem interface

---
+++ Exe34/Entities/Invoice.cs
@@ -0,0 +1,27 @@
+﻿namespace Exe34.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return $"INVOICE:\n" +
+                $"Basic payment: {BasicPayment:F2}\n" +
+                $"Tax: {Tax:F2}\n" +
+                $"Total Payment: {TotalPayment:F2}";
+        }
+    }
+}
